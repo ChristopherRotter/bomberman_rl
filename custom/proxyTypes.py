fb09dab0eb815122654e87d5151a7c74171b2bc1
Christopher Rotter <c.rotter@seitcom.de>
2022-04-20 10:52:26 +0200

    Copied template for DQN agent

---
+++ custom/proxyTypes.py
@@ -0,0 +1,14 @@
+from ast import Pass
+
+
+class GameLogger():
+
+    def info(message : str):
+        pass
+
+    def debug(message : str):
+        Pass
+
+class GameContext():
+
+    logger : GameLogger
